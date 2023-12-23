a63be07e6011200cf54b582f6edb78b5fbbd76d4
Samip Poudel <144611884+poudelsamip@users.noreply.github.com>
2023-12-23 13:51:59 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function showans1(){
+    const show = document.querySelector('.ans1')
+    show.style.display = "block";
+}
+function hideans1(){
+    const show = document.querySelector('.ans1')
+    show.style.display = "none";
+}
+function showans2(){
+    const show = document.querySelector('.ans2')
+    show.style.display = "block";
+}
+
+function hideans2(){
+    const show = document.querySelector('.ans2')
+    show.style.display = "none";
+}
+
+function showans3(){
+    const show = document.querySelector('.ans3')
+    show.style.display = "block";
+}
+
+function hideans3(){
+    const show = document.querySelector('.ans3')
+    show.style.display = "none";
+}
+
+function showans4(){
+    const show = document.querySelector('.ans4')
+    show.style.display = "block";
+}
+
+function hideans4(){
+    const show = document.querySelector('.ans4')
+    show.style.display = "none";
+}
+
+function showans5(){
+    const show = document.querySelector('.ans5')
+    show.style.display = "block";
+}
+
+function hideans5(){
+    const show = document.querySelector('.ans5')
+    show.style.display = "none";
+}
